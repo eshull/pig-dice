6cbeaf2f93701a2858564d5616ef81ecb17490c7
Eric Shull and Kelli Brooke <eric@>
2018-06-13 11:43:13 -0700

    add prototypes for player

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,66 @@ function Player(name){
   this.score = [];
 }
 
-function rollDice(){
+Player.prototype.roll(){
   var roll = Math.floor((Math.random() * 6) +1);
   console.log(roll);
   return roll;
-} /* ROLL DICE */
-
-function testRoll(roll){
-  var turnScore = roll;
-  if (roll === 1) {
-    turnScore = 0;
-  }
-
-
-Player.prototype.rollDice = function () {
-  var roll = Math.floor((Math.random() * 6) +1);
-  console.log(roll);
-  return roll;
-
-
-Player.prototype.score = function() {
-  return this.turnscore
 }
+
+Player.prototype.runningTotal = function(number){
+    var turnTotal = [];
+    turnTotal.push(number);
+}
+
+Player.prototype.confirmHold = function(){
+  var holdStatus = confirm("Do you want to hold?");
+  return holdStatus;
+}
+
+while (holdStatus === true)
+
+
+
+var Fred = new Player("Fred");
+
+
+// function rollDice(){
+//
+// } /* ROLL DICE */
+//
+// function turn(roll){
+//   var turnScore = roll;
+//   hold = false
+// while (hold === false) {
+//   if (roll === 1) {
+//     turnScore = 0;
+//     return turnScore;
+//   } else {
+//     turnScore = roll + turnScore;
+//   }
+//   var hold = confirm("do you want to continue?")
+//   if (true) {
+//     hold = return turnScore;
+//   } else if (false) {
+//     hold = false;
+//   }
+// }
+
+// Player.prototype.rollDice = function () {
+//   var roll = Math.floor((Math.random() * 6) +1);
+//   console.log(roll);
+//   return roll;
+
+
 
 
 
 // UI LOGIC
 $(function(){
+
+  var diceValue = roll();
+  turnTotal(diceValue);
+
 
 
 

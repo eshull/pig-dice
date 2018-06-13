f97af72208744a1558cb47b4c3672908a68e9edb
Eric Shull and Kelli Brooke <eric@>
2018-06-13 10:36:10 -0700

    add beginning of business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
+// BUSINESS LOGIC
+function Player(name){
+  this.name = name;
+  this.score = [];
+}
+
+function rollDice(){
+  var roll = Math.floor((Math.random() * 6) +1);
+  console.log(roll);
+  return roll;
+} /* ROLL DICE */
+
+function testRoll(roll){
+  var turnScore = roll;
+  if (roll === 1) {
+    turnScore = 0;
+  }
+
+
+Player.prototype.rollDice = function () {
+  var roll = Math.floor((Math.random() * 6) +1);
+  console.log(roll);
+  return roll;
+
+
+Player.prototype.score = function() {
+  return this.turnscore
+}
+
+
+
+// UI LOGIC
 $(function(){
 
 

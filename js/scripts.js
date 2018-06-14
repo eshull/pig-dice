4542f0d61c3f061acbcf54e5067609a4aa04f747
Eric Shull and Kelli Brooke <eric@>
2018-06-14 11:12:29 -0700

    build un-refactored site

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,142 @@
 // BUSINESS LOGIC
 function Player(name) {
   this.name = name;
+  // this.dieroll = 0
   this.runningScore = 0;
+  this.totalScore = 0;
 }
-
-// Player.prototype.roll = function() {
-//   var diceRoll = Math.floor((Math.random() * 6) +1);
-//   return diceRoll;
-// }
 
 Player.prototype.sendScore = function(singleRoll) {
   this.runningScore = this.runningScore + singleRoll;
 }
-// pushing the die value and returning a true or false
-Player.prototype.singleTurn = function() {
-  var roll = Math.floor((Math.random() * 6) +1);
-  console.log("function called");
-  if(roll === 1) {
-    this.sendScore(0);
-    return false;
-  } else {
-    this.sendScore(roll);
-    return true;
+
+Player.prototype.roll = function(){
+var roll = Math.floor((Math.random() * 6) +1);
+return roll;
+}
+
+Player.prototype.check100 = function(){
+  if(this.totalScore >= 100){
+    alert(this.name + ", you won!");
   }
+}
 
-  }
-
-  Player.prototype.continuePopUp = function(){
-    var holdStatus = confirm('Hold?');
-    return holdStatus;
-  }
-
-  function endTurn(div1, div2){
-    div1.hide();
-    div2.show();
-  }
-
-
-// function endTurn(){}
-// if (singleTurnVar === false) {
-//   hide current player area
-//   add current players final turn score to score div
-//
-// } else {
-//   var newRoll = currentPlayer.roll();
-//   confirm()
-//   singleTurn(roll);
-// }
 
 
 // UI LOGIC
 $(function(){
-  var player1 = new Player("Kelli");
-  var player2 = new Player("Eric");
+  var player1 = new Player();
+  var player2 = new Player();
   var player1Div = $("#playerOneArea");
   var player2Div = $("#playerTwoArea");
   var startGameDiv = $(".startGame");
 
   $("#play-button").click(function(){
     event.preventDefault();
-    endTurn(startGameDiv, player1Div);
+    $("#startGame").hide();
+    $("#playerOneArea").show();
+    player1.name = $("#playerOne").val();
+    $("#player1-name").text(player1.name);
+    player2.name = $("#playerTwo").val();
+    $("#player2-name").text(player2.name);
+    // endTurn(startGameDiv, player1Div);
   })
 
-  $("#player1-button").click(function(){
-    var singleTurnVar = player1.singleTurn();
-    if (singleTurnVar === false){
-      // endTurn();
+  $("#player1-roll-button").click(function(){
+    var diceValue = player1.roll();
+    $("#p1-current-roll-area").text(diceValue);
+    console.log(player1.totalScore);
+    // function
+    // jquery grab runningscore
+    if(diceValue === 1){
+      player1.runningScore = 0;
+      var runningScore = player1.runningScore;
+      $("#p1-running-score-area").text(runningScore);
+      player1.totalScore += player1.runningScore;
+      console.log(player1.totalScore);
+      $("#p1-total-score-area").text(player1.totalScore);
+      player1.runningScore = 0;
+      $("#p1-running-score-area").text(player1.runningScore);
+      $("#playerOneArea").hide();
+      $("#playerTwoArea").fadeIn();
+      console.log(player1.totalScore);
+      player1.totalScore += runningScore;
     } else {
-      var hold = player1.continuePopUp();
-      while (hold === true) {
-        player1.singleTurn();
-        hold = player1.continuePopUp();
-      }
+      player1.runningScore += diceValue;
+      runningScore = player1.runningScore;
+      $("#p1-running-score-area").text(runningScore);
+      console.log(player1.totalScore);
     }
+
+  })
+
+  $("#player2-roll-button").click(function(){
+    var diceValue = player2.roll();
+    $("#p2-current-roll-area").text(diceValue);
+    console.log(player2.totalScore);
+    if(diceValue === 1){
+      player2.runningScore = 0;
+      var runningScore = player2.runningScore;
+      $("#p2-running-score-area").text(runningScore);
+      player2.totalScore += player2.runningScore;
+      console.log(player2.totalScore);
+      $("#p2-total-score-area").text(player2.totalScore);
+      player2.runningScore = 0;
+      $("#p2-running-score-area").text(player2.runningScore);
+      $("#playerTwoArea").hide();
+      $("#playerOneArea").fadeIn();
+      console.log(player2.totalScore);
+      player2.totalScore += runningScore;
+    } else {
+      player2.runningScore += diceValue;
+      runningScore = player2.runningScore;
+      $("#p2-running-score-area").text(runningScore);
+      console.log(player2.totalScore);
+    }
+
+  })
+    // var singleTurnVar = player1.singleTurn();
+    // var displayRoll = player1.currentRoll();
+    // if (singleTurnVar === false){
+    //   // endTurn();
+    // } else {
+    //   var hold = player1.continuePopUp();
+    //   while (hold === true) {
+    //     player1.singleTurn();
+    //     hold = player1.continuePopUp();
+    //   }
+    // }
+
+
+    $("#player1-hold-button").click(function(){
+      event.preventDefault();
+      console.log("clicked");
+      player1.totalScore += player1.runningScore;
+      player1.check100();
+      console.log(player1.totalScore);
+      $("#p1-total-score-area").text(player1.totalScore);
+      $("#p1-opponent-score-area").text(player2.totalScore);
+      $("#p2-opponent-score-area").text(player1.totalScore);
+      player1.runningScore = 0;
+      $("#running-score-area").text(player1.runningScore);
+      $("#playerOneArea").hide();
+      $("#playerTwoArea").fadeIn();
+    })
+
+    $("#player2-hold-button").click(function(){
+      event.preventDefault();
+      console.log("player 2 clicked");
+      player2.totalScore += player2.runningScore;
+      player2.check100();
+      console.log(player2.totalScore);
+      $("#p2-total-score-area").text(player2.totalScore);
+      $("#p2-opponent-score-area").text(player1.totalScore);
+      $("#p1-opponent-score-area").text(player2.totalScore);
+      player2.runningScore = 0;
+      $("#p2-running-score-area").text(player2.runningScore);
+      $("#playerTwoArea").hide();
+      $("#playerOneArea").fadeIn();
+    })
     // var confirm()
     // if(holdPrompt === true){
     //   player1.sendScore();
@@ -80,7 +144,7 @@ $(function(){
     //   singleTurn(roll);
     // }
 
-  })
+
 
 
 
